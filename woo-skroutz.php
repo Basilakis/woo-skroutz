f0087002c51100dccfbeaa4c3903efa889527d3f
Aristeides Stathopoulos <aristath@gmail.com>
2018-03-09 22:06:27 +0200

    Add main plugin file

---
+++ woo-skroutz.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Woo Skroutz
+ * Plugin URI: https://wpmu.io
+ * Description: Add Skroutz.gr XML generation per-product.
+ * Author: Aristeides Stathopoulos
+ * Author URI: https://aristath.github.io
+ * Developer: WPMU.IO
+ * Developer URI: https://wpmu.io
+ * Requires at least: 4.9
+ * Tested up to: 4.9
+ * Version: 1.0
+ * Text Domain: skroutz
+ */
